fc5ad1899aa18c7ae9884227fdc645c9b13f629b
Alexius Academia <alexius.academia@gmail.com>
2023-02-28 16:05:14 +0800

    Display help file from markdown

--- helper/help_dialog.py
+++ helper/help_dialog.py
@@ -1,8 +1,27 @@
 import wx
+import wx.html2
+import markdown
+
+import os
 
 
 class HelpDialog(wx.Dialog):
     def __init__(self, parent, title='Help'):
         super().__init__(parent, title=title)
 
+        self.panel = wx.Panel(self)
 
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        print(dir_path)
+
+        with open(f"{dir_path}/readme.md", "r") as f:
+            markdown_text = f.read()
+
+        html_text = markdown.markdown(markdown_text)
+
+        self.webview = wx.html2.WebView.New(self.panel)
+        self.webview.SetPage(html_text, "")
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.webview, 1, wx.EXPAND)
+        self.panel.SetSizer(sizer)
---
+++ helper/readme.md
@@ -0,0 +1,14 @@
+# ExifViewer
+
+This is a simple tool to view exif data (specifically GPS coordinates) of 
+a picture. This is an open source project that aims to demonstrate the use
+of `wxPython` for a GUI project.
+
+## How To Use
+
+This is a very early version and not much feature is yet written on it. To 
+use this, download the release file. Then on the executable, place an image
+with a filename `image.jpg`. In this version, the exact filename should be 
+followed in order to work.
+
+
